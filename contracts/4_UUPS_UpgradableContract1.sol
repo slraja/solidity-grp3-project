8e14e3b921c1efe9082225a06e6d659a9398a78b
slraja <slraja83@gmail.com>
2024-10-02 18:34:49 +0100

    rename UUPS contracts

--- contracts/4_UUPS_UpgradableContract1.sol
+++ contracts/4_UUPS_UpgradableContract1.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract Group3_UpgradableContract is 
+contract UUPS_UpgradableContract2 is 
                             UUPSUpgradeable,
                             OwnableUpgradeable 
                             {
@@ -26,7 +26,7 @@ contract Group3_UpgradableContract is
 
 
      function updateNumber(uint _number) public returns (uint, uint) {
-        number = _number;
+        number = _number * 10 ;
         changeId.increment();
         uint id = changeId.current();
         return (number, id);
