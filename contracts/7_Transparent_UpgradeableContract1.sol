7a359a04eccd9d1a16a2755410302ce2a64e20bd
slraja <slraja83@gmail.com>
2024-10-03 16:43:23 +0100

    commit wip - Transparent Proxy

---
+++ contracts/7_Transparent_UpgradeableContract1.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+
+contract Transparent_UpgradeableContract1 is Initializable
+ {
+    uint256 public number;
+
+    function initialize(uint256 initialValue) public initializer{
+    number = initialValue;
+    }
+
+
+    function setNumber(uint256 newNumber) public {
+        number = newNumber;
+    }
+
+    function incrementNumber() public {
+        number += 1;
+    }
+
+    function getNumber() public view returns (uint256) {
+        return number;
+    }
+}
